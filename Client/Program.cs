fae0b0da55eeed21c2fdf8229190461035bf02b2
Vyacheslav Goryushkin <vyacheslav.goryushkin@altkraft.com>
2021-10-19 00:27:44 +0300

    add Blazorise

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OnlineStore.Client;
@@ -5,6 +8,11 @@ using OnlineStore.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
